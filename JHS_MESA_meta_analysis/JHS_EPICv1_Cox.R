e1f76324bc811130e127a13308bdd29a82022398
Hazel Milla <155584697+hazelmilla@users.noreply.github.com>
2026-05-08 16:24:06 -0400

    Update JHS_EPICv1_Cox.R

--- JHS_MESA_meta_analysis/JHS_EPICv1_Cox.R
+++ JHS_MESA_meta_analysis/JHS_EPICv1_Cox.R
@@ -6,11 +6,13 @@ library(dplyr)
 dir = "/Users/hazelmilla/Desktop/Zannas_lab/MRS_WHI_JHS/V1/"
 setwd(dir)
 
+#######################################
+###### Threshold 1 (FDR) #######
+#######################################
+
 data_unfiltered <- read.csv("Input_files/chd_data.csv", header = TRUE)
 dim(data_unfiltered)
 #[1] 1554   40
-
-####### FDR THRESHOLD ########
 
 # Filter data
 data <- data_unfiltered %>% drop_na(CHD) %>% drop_na(days_to_event) %>%
@@ -26,7 +28,7 @@ data$MRS2cat <- factor(data$MRS2cat,
                              levels = c(1, 2), 
                              labels = c("Low MRS", "High MRS"))
 
-##### Fit Cox proportional hazards model #####
+###### Fit Cox proportional hazards model ######
 
 cox_model <- coxph(Surv(days_to_event, CHD) ~ MRS2cat + sex + age + 
                      currentSmoker + BMI + Income + edu3cat + alc + 
@@ -61,18 +63,6 @@ surv_obj <- Surv(data$days_to_event, data$CHD == 1)
 # Fit Kaplan-Meier survival curves
 km_fit <- survfit(surv_obj ~ MRS2cat, data = data)
 
-survival_plot <- ggsurvplot(km_fit, 
-                            data = data, 
-                            pval = TRUE, 
-                            conf.int = TRUE,
-                            legend.labs = c("Low", "High"),
-                            legend.title = "MRS Level",
-                            title = "CHD Risk by MRS level",
-                            xlab = "Time to Event in Days",
-                            ylab = "CHD Probability")$plot
-
-survival_plot
-
 event_plot <- ggsurvplot(km_fit, 
                          data = data, 
                          pval = TRUE, 
@@ -86,7 +76,10 @@ event_plot <- ggsurvplot(km_fit,
 
 event_plot
 
-###### BONFERRONI THRESHOLD #########
+#######################################
+###### Threshold 2 (Bonferroni) #######
+#######################################
+
 data <- data_unfiltered %>% drop_na(CHD) %>% drop_na(days_to_event) %>%
   drop_na(sex) %>% drop_na(age) %>%
   drop_na(currentSmoker) %>% drop_na(BMI) %>% drop_na(Income) %>% 
@@ -100,7 +93,7 @@ data$MRS2cat_bonferroni <- factor(data$MRS2cat_bonferroni,
                            levels = c(1, 2), 
                            labels = c("Low MRS", "High MRS"))
 
-##### Fit Cox proportional hazards model #####
+###### Fit Cox proportional hazards model ######
 cox_model <- coxph(Surv(days_to_event, CHD) ~ MRS2cat_bonferroni + sex + age + 
                      currentSmoker + BMI + Income + edu3cat + alc + 
                      marital2cat + nbSESanascore +
@@ -133,18 +126,6 @@ surv_obj <- Surv(data$days_to_event, data$CHD == 1)
 
 # Fit Kaplan-Meier survival curves
 km_fit <- survfit(surv_obj ~ MRS2cat_bonferroni, data = data)
-
-survival_plot <- ggsurvplot(km_fit, 
-                            data = data, 
-                            pval = TRUE, 
-                            conf.int = TRUE,
-                            legend.labs = c("Low", "High"),
-                            legend.title = "MRS Level",
-                            title = "CHD Risk by MRS level",
-                            xlab = "Time to Event in Days",
-                            ylab = "CHD Probability")$plot
-
-survival_plot
 
 event_plot <- ggsurvplot(km_fit, 
                          data = data, 
