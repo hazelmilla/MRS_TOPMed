37a62f74f4d48816371aa247308b0232fc2b5d9a
Hazel Milla <155584697+hazelmilla@users.noreply.github.com>
2026-05-11 11:42:44 -0400

    Update JHS_Cox.R

--- JHS_MESA_meta_analysis/JHS/JHS_Cox.R
+++ JHS_MESA_meta_analysis/JHS/JHS_Cox.R
@@ -2,320 +2,224 @@
 ###### JHS EPICv1 Cox regression #######
 ########################################
 
-library(survival)
-library(survminer)
-library(tidyverse)
-library(dplyr)
-
-dir = "/Users/hazelmilla/Desktop/Zannas_lab/MRS_WHI_JHS/V1/"
+# Get CHD data and load packages
+dir = "~/Library/CloudStorage/OneDrive-UniversityofNorthCarolinaatChapelHill/002 Zannas_lab/MRS TOPMed/MRS_WHI_JHS/V1/"
 setwd(dir)
-
-#######################################
-###### Threshold 1 (FDR) #######
-#######################################
 
 data_unfiltered <- read.csv("Input_files/chd_data.csv", header = TRUE)
 dim(data_unfiltered)
 #[1] 1554   40
 
-# Filter data
-data <- data_unfiltered %>% drop_na(CHD) %>% drop_na(days_to_event) %>%
-  drop_na(sex) %>% drop_na(age) %>%
-  drop_na(currentSmoker) %>% drop_na(BMI) %>% drop_na(Income) %>% 
-  drop_na(edu3cat) %>% drop_na(alc) %>% drop_na(marital2cat) %>% 
-  drop_na(nbSESanascore) %>% drop_na(MRS2cat)
+library(dplyr)
+library(tidyverse)
+library(ggpubr)
+library(ggplot2)
+library(rstatix)
+library(broom)
+library(emmeans)
+library(survminer)
+library(survival)
+
+######## THRESHOLD 1 (FDR) ########
+# Model - sex, age, smoking, bmi, income, education, alc, marital status,
+# SES, PC1-10, Houseman cell types
+variables_JHS <- c("MRS", "sex", "age", "currentSmoker", "BMI", "Income",
+                   "edu3cat", "alc", "marital2cat", "nbSESanascore",
+                   "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8",
+                   "PC9", "PC10", "NK", "Mono", "Gran", "Bcell", "CD8T",
+                   "CD4T") # create list of variables to remove NAs
+
+# Remove NAs
+data <- data_unfiltered[complete.cases(data_unfiltered[ , variables_JHS]), ]
 dim(data)
-#[1] 1291   40
+# [1] 1321   40
+sum(data$CHD == 1, na.rm = TRUE) #97
 
-# Ensure MRS with median split is a factor with proper labels
-data$MRS2cat <- factor(data$MRS2cat, 
-                             levels = c(1, 2), 
-                             labels = c("Low MRS", "High MRS"))
+# PLOT RESIDUALS
+model_resid <- lm(data$MRS ~ sex + age + currentSmoker + BMI +
+                    Income + edu3cat + alc + marital2cat + nbSESanascore +
+                    PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
+                    PC8 + PC9 + PC10 + NK + Mono + Gran + 
+                    Bcell + CD8T + CD4T, data=data)
 
-###### Fit Cox proportional hazards model ######
+data$MRS_resid <- resid(model_resid)
 
-cox_model <- coxph(Surv(days_to_event, CHD) ~ MRS2cat + sex + age + 
-                     currentSmoker + BMI + Income + edu3cat + alc + 
-                     marital2cat + nbSESanascore + 
-                     PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
-                     PC8 + PC9 + PC10 + NK + Mono + Gran + 
-                     Bcell + CD8T + CD4T, 
+MRS_median <- summary(data$MRS_resid)[3]
+data$MRSresid2cat[data$MRS_resid <= MRS_median] <- 1
+data$MRSresid2cat[data$MRS_resid > MRS_median] <- 2
+table(data$MRSresid2cat)
+#1 - 709    2 - 708
+v1_fdr_all <- data
+
+cox_model <- coxph(Surv(days_to_event, CHD) ~ MRSresid2cat, 
                    data = data)
 
-# Summarize the model
-coefficients <- summary(cox_model)$coefficients
-colnames(coefficients) <- c("Estimate", "OR", "SE",
-                            "z", "P_val")
-
-setwd(outputdir)
-write.csv(coefficients, file = "Output_files/v1_cox_results_fdr.csv")
-
-# check assumptions
-test_ph <- cox.zph(cox_model)
-print(test_ph)
-# if p-value is significant, stratify by that variable
-# no p values are significant
-
-ggforest(cox_model, data = data) # visualize results
-
-####### Generate Kaplan-Meier curve #########
-
-setwd(inputdir)
-# Create survival object
-surv_obj <- Surv(data$days_to_event, data$CHD == 1)
-
-# Fit Kaplan-Meier survival curves
-km_fit <- survfit(surv_obj ~ MRS2cat, data = data)
-
-event_plot <- ggsurvplot(km_fit, 
-                         data = data, 
-                         pval = TRUE, 
-                         conf.int = TRUE,
-                         fun = "event",  # This inverts to show event probability
-                         legend.labs = c("Low", "High"),
-                         legend.title = "MRS Level",
-                         title = "CHD Cumulative Incidence by MRS level",
-                         xlab = "Time to Event in Days",
-                         ylab = "CHD Probability")$plot
-
-event_plot
-
-#######################################
-###### Threshold 2 (Bonferroni) #######
-#######################################
-
-data <- data_unfiltered %>% drop_na(CHD) %>% drop_na(days_to_event) %>%
-  drop_na(sex) %>% drop_na(age) %>%
-  drop_na(currentSmoker) %>% drop_na(BMI) %>% drop_na(Income) %>% 
-  drop_na(edu3cat) %>% drop_na(alc) %>% drop_na(marital2cat) %>% 
-  drop_na(nbSESanascore) %>% drop_na(MRS2cat_bonferroni)
-dim(data)
-#[1] 1291   39
-
-# Ensure MRS_med_split is a factor with proper labels
-data$MRS2cat_bonferroni <- factor(data$MRS2cat_bonferroni, 
-                           levels = c(1, 2), 
-                           labels = c("Low MRS", "High MRS"))
-
-###### Fit Cox proportional hazards model ######
-cox_model <- coxph(Surv(days_to_event, CHD) ~ MRS2cat_bonferroni + sex + age + 
-                     currentSmoker + BMI + Income + edu3cat + alc + 
-                     marital2cat + nbSESanascore +
-                     PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
-                     PC8 + PC9 + PC10 + NK + Mono + Gran + 
-                     Bcell + CD8T + CD4T, 
-                   data = data)
-
-# check assumptions of the model
 test_ph <- cox.zph(cox_model)
 print(test_ph)
 
-print(ifelse(test_ph$table[,3] < 0.05, "yes", "no")[test_ph$table[,3] < 0.05])
-# if p-value is significant, you need to stratify by that variable
-# no p values are significant
+# Summarize the model
+coefficients <- summary(cox_model)$coefficients
+colnames(coefficients) <- c("Estimate", "HR", "SE",
+                            "z", "P_val")
+v1_fdr_cox <- coefficients
+
+setwd("/Users/hazelmilla/Desktop/Zannas_lab/JHS_MESA_meta_analysis/Input")
+write.csv(coefficients, file = "v1_cox_results_fdr.csv")
+
+######### THRESHOLD 2 (Bonferroni) ########
+
+# Model - sex, age, smoking, bmi, income, education, alc, marital status,
+# SES, PC1-10, Houseman cell types
+variables_JHS <- c("MRS_bonferroni", "sex", "age", "currentSmoker", "BMI", "Income",
+                   "edu3cat", "alc", "marital2cat", "nbSESanascore",
+                   "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8",
+                   "PC9", "PC10", "NK", "Mono", "Gran", "Bcell", "CD8T",
+                   "CD4T") # create list of variables to remove NAs
+# Remove NAs
+data <- data_unfiltered[complete.cases(data_unfiltered[ , variables_JHS]), ]
+dim(data)
+# [1] 1321   40
+
+# PLOT RESIDUALS
+model_resid <- lm(data$MRS_bonferroni ~ sex + age + currentSmoker + BMI +
+                    Income + edu3cat + alc + marital2cat + nbSESanascore +
+                    PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
+                    PC8 + PC9 + PC10 + NK + Mono + Gran + 
+                    Bcell + CD8T + CD4T, data=data)
+
+data$MRS_resid <- resid(model_resid)
+
+MRS_median <- summary(data$MRS_resid)[3]
+data$MRSresid2cat[data$MRS_resid <= MRS_median] <- 1
+data$MRSresid2cat[data$MRS_resid > MRS_median] <- 2
+table(data$MRSresid2cat)
+#1 - 709    2 - 708
+v1_bonf_all <- data
+
+cox_model <- coxph(Surv(days_to_event, CHD) ~ MRSresid2cat, 
+                   data = data)
+
+test_ph <- cox.zph(cox_model)
+print(test_ph)
 
 # Summarize the model
 coefficients <- summary(cox_model)$coefficients
-colnames(coefficients) <- c("Estimate", "OR", "SE",
+colnames(coefficients) <- c("Estimate", "HR", "SE",
                             "z", "P_val")
+v1_bonf_cox <- coefficients
 
-write.csv(coefficients, file = "Output_files/v1_cox_results_bonferroni.csv")
-
-ggforest(cox_model, data = data) # visualize results
-
-####### Generate Kaplan-Meier curve #########
-
-# Create survival object
-surv_obj <- Surv(data$days_to_event, data$CHD == 1)
-
-# Fit Kaplan-Meier survival curves
-km_fit <- survfit(surv_obj ~ MRS2cat_bonferroni, data = data)
-
-event_plot <- ggsurvplot(km_fit, 
-                         data = data, 
-                         pval = TRUE, 
-                         conf.int = TRUE,
-                         fun = "event",  # This inverts to show event probability
-                         legend.labs = c("Low", "High"),
-                         legend.title = "MRS Level",
-                         title = "CHD Cumulative Incidence by MRS level",
-                         xlab = "Time to Event in Days",
-                         ylab = "CHD Probability")$plot
-
-event_plot
-
+setwd("/Users/hazelmilla/Desktop/Zannas_lab/JHS_MESA_meta_analysis/Input")
+write.csv(v1_bonf_cox, file = "v1_cox_results_bonf.csv")
 
 ########################################
 ###### JHS EPICv2 Cox regression #######
 ########################################
 
+dir = "/Users/hazelmilla/Desktop/Zannas_lab/MRS_WHI_JHS/V2"
+setwd(dir)
 
-# Cox regression - v2 data
-library(survival)
-library(survminer)
-library(tidyverse)
+data_unfiltered <- read.csv("chd_data_v2.csv", header = TRUE)
+dim(data_unfiltered)
+#[1] 1499   43
+
 library(dplyr)
+library(tidyverse)
+library(ggpubr)
 library(ggplot2)
+library(rstatix)
+library(broom)
+library(emmeans)
+library(survminer)
+library(survival)
 
-path = "/Users/hazelmilla/Desktop/Zannas_lab/MRS_WHI_JHS/V2"
-setwd(path)
-
-# Fit a Cox proportional hazards model
-# Plot survival time based on MRS & covariates
-
-#######################################
-###### Threshold 1 (FDR) #######
-#######################################
-
-data_unfiltered <- read.csv("chd_data_v2.csv", header = TRUE)
-
-data <- data_unfiltered %>% drop_na(MRS2cat) %>% drop_na(CHD) %>% drop_na(days_to_event) %>%
-  drop_na(sex) %>% drop_na(age) %>%
-  drop_na(currentSmoker) %>% drop_na(BMI) %>% drop_na(Income) %>% 
-  drop_na(edu3cat) %>% drop_na(alc) %>% drop_na(marital2cat) %>% 
-  drop_na(nbSESanascore)
+######## THRESHOLD 1 (FDR) ########
+# Model - sex, age, smoking, bmi, income, education, alc, marital status,
+# SES, PC1-10, Houseman cell types
+variables_JHS <- c("MRS", "sex", "age", "currentSmoker", "BMI", "Income",
+                   "edu3cat", "alc", "marital2cat", "nbSESanascore",
+                   "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8",
+                   "PC9", "PC10", "NK", "Mono", "Gran", "Bcell", "CD8T",
+                   "CD4T") # create list of variables to remove NAs
+data <- data_unfiltered[complete.cases(data_unfiltered[ , variables_JHS]), ]
 dim(data)
-#[1] 1202   43
+# [1] 1227   43
+sum(data$CHD == 1, na.rm = TRUE) #70
 
-# Ensure MRS_med_split is a factor with proper labels
-data$MRS2cat <- factor(data$MRS2cat, 
-                       levels = c(1, 2), 
-                       labels = c("Low MRS", "High MRS"))
+# PLOT RESIDUALS
+model_resid <- lm(data$MRS ~ sex + age + currentSmoker + BMI +
+                    Income + edu3cat + alc + marital2cat + nbSESanascore +
+                    PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
+                    PC8 + PC9 + PC10 + NK + Mono + Gran + 
+                    Bcell + CD8T + CD4T, data=data)
 
-data$BMI3cat <- ntile(data$BMI, 3) # Split into tertiles due to assumption violation (p < 0.05)
-data$NK3cat <- ntile(data$NK, 3) # Split into tertiles due to assumption violation (p < 0.05)
-data$Gran3cat <- ntile(data$Gran, 3) # Split into tertiles due to assumption violation (p < 0.05)
+data$MRS_resid <- resid(model_resid)
 
-######## Fit Cox proportional hazards regression model #########
-cox_model <- coxph(Surv(days_to_event, CHD) ~ MRS2cat + sex + age + 
-                     currentSmoker + strata(BMI3cat) + Income + edu3cat + alc + 
-                     marital2cat + nbSESanascore + 
-                     PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
-                     PC8 + PC9 + PC10 + strata(NK3cat) + Mono + Gran3cat + 
-                     Bcell + CD8T + CD4T, 
+MRS_median <- summary(data$MRS_resid)[3]
+data$MRSresid2cat[data$MRS_resid <= MRS_median] <- 1
+data$MRSresid2cat[data$MRS_resid > MRS_median] <- 2
+table(data$MRSresid2cat)
+#1 - 614    2 - 613
+v2_fdr_all <- data
+
+cox_model <- coxph(Surv(days_to_event, CHD) ~ MRSresid2cat, 
                    data = data)
 
-
-# check assumptions
 test_ph <- cox.zph(cox_model)
 print(test_ph)
-
-print(ifelse(test_ph$table[,3] < 0.05, "yes", "no")[test_ph$table[,3] < 0.05])
-# if p-value is significant, stratify by that variable
-# p-values < 0.05 for Gran, NK, and BMI
-
-# Summarize the model
-cox_summary <- summary(cox_model)
 
 # Summarize the model
 coefficients <- summary(cox_model)$coefficients
-colnames(coefficients) <- c("Estimate", "OR", "SE",
+colnames(coefficients) <- c("Estimate", "HR", "SE",
                             "z", "P_val")
 
-write.csv(coefficients, file = "v2_cox_results_fdr.csv")
+v2_fdr_cox <- coefficients
 
-ggforest(cox_model, data = data)
+setwd("/Users/hazelmilla/Desktop/Zannas_lab/JHS_MESA_meta_analysis/Input")
+write.csv(v2_fdr_cox, file = "v2_cox_results_fdr.csv")
 
-####### Generate Kaplan-Meier curve #########
 
-# Create survival object
-surv_obj <- Surv(data$days_to_event, data$CHD == 1)
+######## THRESHOLD 2 (BONFERRONI) ########
+# Model - sex, age, smoking, bmi, income, education, alc, marital status,
+# SES, PC1-10, Houseman cell types
+variables_JHS <- c("MRS_bonferroni", "sex", "age", "currentSmoker", "BMI", "Income",
+                   "edu3cat", "alc", "marital2cat", "nbSESanascore",
+                   "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8",
+                   "PC9", "PC10", "NK", "Mono", "Gran", "Bcell", "CD8T",
+                   "CD4T") # create list of variables to remove NAs
 
-# Fit Kaplan-Meier survival curves
-km_fit <- survfit(surv_obj ~ stress2cat, data = data)
 
-event_plot <- ggsurvplot(km_fit, 
-                         data = data, 
-                         pval = TRUE, 
-                         conf.int = TRUE,
-                         fun = "event",  # This inverts to show event probability
-                         legend.labs = c("Low", "High"),
-                         legend.title = "Stress Level",
-                         title = "CHD Cumulative Incidence by Stress level",
-                         xlab = "Time to Event in Days",
-                         ylab = "CHD Probability")$plot
-
-event_plot
-
-#######################################
-###### Threshold 2 (Bonferroni) #######
-#######################################
-
-data_unfiltered <- read.csv("chd_data_v2.csv", header = TRUE)
-
-data <- data_unfiltered %>% drop_na(MRS2cat_bonf) %>% drop_na(CHD) %>% drop_na(days_to_event) %>%
-  drop_na(sex) %>% drop_na(age) %>%
-  drop_na(currentSmoker) %>% drop_na(BMI) %>% drop_na(Income) %>% 
-  drop_na(edu3cat) %>% drop_na(alc) %>% drop_na(marital2cat) %>% 
-  drop_na(nbSESanascore)
+data <- data_unfiltered[complete.cases(data_unfiltered[ , variables_JHS]), ]
 dim(data)
-#[1] 1202   43
+# [1] 1227   43
 
-# Ensure MRS_med_split is a factor with proper labels
-data$MRS2cat_bonf <- factor(data$MRS2cat_bonf, 
-                       levels = c(1, 2), 
-                       labels = c("Low MRS", "High MRS"))
+# PLOT RESIDUALS
+model_resid <- lm(data$MRS_bonferroni ~ sex + age + currentSmoker + BMI +
+                    Income + edu3cat + alc + marital2cat + nbSESanascore +
+                    PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
+                    PC8 + PC9 + PC10 + NK + Mono + Gran + 
+                    Bcell + CD8T + CD4T, data=data)
 
-data$BMI3cat <- ntile(data$BMI, 3)
-data$NK3cat <- ntile(data$NK, 3)
-data$Gran3cat <- ntile(data$Gran, 3)
+data$MRS_resid <- resid(model_resid)
 
-######## Fit Cox proportional hazards regression model #########
-cox_model <- coxph(Surv(days_to_event, CHD) ~ MRS2cat_bonf + sex + age + 
-                     currentSmoker + strata(BMI3cat) + Income + edu3cat + alc + 
-                     marital2cat + nbSESanascore + 
-                     PC1 + PC2 +  PC3 + PC4 + PC5 + PC6 + PC7 + 
-                     PC8 + PC9 + PC10 + strata(NK3cat) + Mono + Gran3cat + 
-                     Bcell + CD8T + CD4T, 
+MRS_median <- summary(data$MRS_resid)[3]
+data$MRSresid2cat[data$MRS_resid <= MRS_median] <- 1
+data$MRSresid2cat[data$MRS_resid > MRS_median] <- 2
+table(data$MRSresid2cat)
+#1 - 614    2 - 613
+v2_bonf_all <- data
+
+cox_model <- coxph(Surv(days_to_event, CHD) ~ MRSresid2cat, 
                    data = data)
 
-# check assumptions
 test_ph <- cox.zph(cox_model)
 print(test_ph)
-
-print(ifelse(test_ph$table[,3] < 0.05, "yes", "no")[test_ph$table[,3] < 0.05])
-# BMI, NK, and Gran are significant - stratify into tertiles
-
-# Summarize the model
-cox_summary <- summary(cox_model)
 
 # Summarize the model
 coefficients <- summary(cox_model)$coefficients
-colnames(coefficients) <- c("Estimate", "OR", "SE",
+colnames(coefficients) <- c("Estimate", "HR", "SE",
                             "z", "P_val")
 
-write.csv(coefficients, file = "v2_cox_results_bonferroni.csv")
+v2_bonf_cox <- coefficients
 
-# In WHI, MRS is negatively associated. Higher in stress group, 
-# closer to 0, less negative, visualize association with stress 
-
-# check assumptions
-test_ph <- cox.zph(cox_model)
-print(test_ph)
-# if p-value is significant, you need to stratify by that variable
-# BMI was significant when treated as a continuous variable,
-# so I split BMI into 3 categories
-# BMI did not have a significant p-value after splitting into tertiles
-
-ggforest(cox_model, data = data)
-
-####### Generate Kaplan-Meier curve #########
-
-# Create survival object
-surv_obj <- Surv(data$days_to_event, data$CHD == 1)
-
-# Fit Kaplan-Meier survival curves
-km_fit <- survfit(surv_obj ~ stress2cat, data = data)
-
-event_plot <- ggsurvplot(km_fit, 
-                         data = data, 
-                         pval = TRUE, 
-                         conf.int = TRUE,
-                         fun = "event",  # This inverts to show event probability
-                         legend.labs = c("Low", "High"),
-                         legend.title = "Stress Level",
-                         title = "CHD Cumulative Incidence by Stress level",
-                         xlab = "Time to Event in Days",
-                         ylab = "CHD Probability")$plot
-
-event_plot
+setwd("/Users/hazelmilla/Desktop/Zannas_lab/JHS_MESA_meta_analysis/Input")
+write.csv(v2_bonf_cox, file = "v2_cox_results_bonf.csv")
